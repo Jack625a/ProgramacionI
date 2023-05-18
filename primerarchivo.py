ed2a64530bffe929d1be509a042ac0584f6bcdd5
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-05-18 19:13:14 -0400

    Sintaxis basica de Python

---
+++ primerarchivo.py
@@ -0,0 +1,3 @@
+#Sintaxis basica de Python
+variable="Hola bienvenidos a Python"
+print(variable)
---
+++ python.py
@@ -0,0 +1,25 @@
+#Sintaxis basica de Python
+nombre="Kevin Omar Arroyo Montaño"
+print(nombre)
+#Tipos de datos en Python
+#1. Tipos de datos simples
+#*Tipos de datos Numericos
+numero=45
+numero2=12.1548
+#Tipos de datos Cadenas de caracteres
+carrera="Sistemas Informaticos"
+semestre="1-2023"
+estudiante='Kevin Arroyo - 1-2023 - Sistemas Informaticos'
+#Tipos de datos Booleanos
+lampara=True
+motor=False
+
+#2. Tipos de datos compuestos
+#Listas: Coleciones de objetos - tipos de datos numericos,cadenas de caracteres, booleANOS
+colores=["rojo","verde","cafe","amarillo","azul"]
+lista=[12,45,78,12.5,96,45]
+estudiantes=["Kevin",2023,True]
+#Tuplas
+docente=("nombre","fecha",69596229)
+#Diccionarios
+carreras={'nomCarrera':"Sistemas Informaticos",'turno':"Noche",'año':2023,"usuarioActivo":True}

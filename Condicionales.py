4c02f85a50e4e5ddaa1e0849881abeffe20205b5
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-06-01 19:41:03 -0400

    Condicionales

---
+++ Condicionales.py
@@ -0,0 +1,16 @@
+#Condicionales en Python
+#if condicion:
+#bloque de codigo
+#else:
+#bloque de codigo
+#Ejercicio 1 - Verificar el numero mayor de 2 numeros
+print("Ejercicio 1 - Verificar el numero mayor de 2 numeros")
+#Asignacion de las 2 variables 
+a=125
+b=125
+if a>b:
+    print("El numero mayor es:",a)
+elif a==b:
+    print("Los dos numeros son iguales")
+else:
+    print("El numero mayor es:",b)

8ad64978945ec54012eda2c6afc82d988c0306cd
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-06-15 19:55:59 -0400

    Bucles

---
+++ Bucles.py
@@ -0,0 +1,32 @@
+#Bucles,
+#Bucles Condicionales Repetitivo
+#1. Bucles Repetitivos Condicionados
+#while condicion:
+# //Codigo
+print("Ejercicio 1. Mostrar los primeros numeros del 1 al 10")
+#Ejercicio 1. Mostrar los primeros numeros del 1 al 10
+numero=1
+while numero<=100:
+    print(numero)
+    numero=numero+1
+
+print("Ejercicio 2. Mostrar los numeros del 100 al 1")
+#Ejercicio 2. Mostrar los numeros del 100 al 1
+numero2=100
+while numero2>=1:
+    print(numero2)
+    numero2=numero2-1
+
+
+#2. Bucles Repetitivos Logicos
+#while True:
+#if condicion:
+#break=> False
+
+
+
+
+
+
+
+#BUcles Iterativos

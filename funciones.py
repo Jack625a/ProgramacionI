baef7d9119e3117e4c4d666d02ed53cc828b1d17
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-06-29 20:12:25 -0400

    Calculadora

---
+++ funciones.py
@@ -0,0 +1,24 @@
+#Funciones def
+#def NombreFuncion(parametros)
+    #Codigo 
+#Ejercicio 1- Crear una funcion para Sumar 2 numeros 
+def Sumar(num1,num2):
+    restultado=num1+num2
+    print("La suma de ",num1,"+",num2,"= ",restultado)
+Sumar(5,8)
+Sumar(45,12)
+a=float(input("Ingrese un numero: "))
+b=float(input("Ingrese un numero: "))
+Sumar(a,b)
+
+#Funciones sin Parametros
+def Saludar():
+    print("Hola")
+
+Saludar()
+Saludar()
+Saludar()
+Saludar()
+Saludar()
+Saludar()
+Saludar()

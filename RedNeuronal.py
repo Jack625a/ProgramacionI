a25752ec746a17d13eeb3a19bdcb7d3ab744d4a1
Jack625a <77822933+Jack625a@users.noreply.github.com>
2023-08-10 20:48:11 -0400

    Conexion Api - Openai

---
+++ RedNeuronal.py
@@ -0,0 +1,25 @@
+#Ejercicio 1 - 10/08/2023
+#Conexion con Apis (RED NEURONAL - OPENAI)
+import openai
+openai.api_key="su apikey"
+
+def obtenerRespuesta(pregunta):
+    respuesta=openai.Completion.create(
+        prompt=pregunta,
+        engine="text-davinci-003",
+        temperature=0.5, #0-1
+        max_tokens=350
+    )
+    return respuesta.choices[0].text.strip()
+#Funcion principal
+while True:
+    opcion=int(input("Desea seguir utilizando el servicio? 1. para Continuar, 2. No para Salir"))
+    if opcion==1:
+        pregunta=input("Hola, cual es tu consulta? ")
+        respuesta=obtenerRespuesta(pregunta)
+        print(respuesta)
+    elif opcion==2:
+        print("Gracias por utilizar el servicio de Inteligencia Artificial")
+        break
+    else:
+        print("Error, operación invalida")
